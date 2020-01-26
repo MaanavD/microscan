3c76d23613011f8c24cad7b82a7e2cd112878b5e
jaymody <jaykmody@gmail.com>
2020-01-25 22:51:41 -0500

    skeleton for imaging scripts

---
+++ imaging.py
@@ -0,0 +1,29 @@
+# imports
+import cv2
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# image preprocessing
+def remove_flare(img):
+    pass
+
+def remove_curtaining(img, curtain_mask):
+    pass
+
+# main procesing
+def get_edges(img):
+    pass
+
+def get_grain_sizes(img):
+    pass
+
+def get_phase_masks(img):
+    pass
+
+def get_phase_fractions(img):
+    pass
+
+# main
+if __name__ == "__main__":
+    pass
